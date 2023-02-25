a86c42378d87da9ab3182645f395c7b1430b20af
Phil Leggetter <phil@leggetter.co.uk>
2023-02-25 11:18:03 +0000

    chore: remove back link in full blog list view

--- src/util/hideNavbarInBlogPost.js
+++ src/util/hideNavbarInBlogPost.js
@@ -28,33 +28,11 @@ const adjustBlogPostUI = (location) => {
       "main[itemtype='http://schema.org/Blog']"
     );
 
-    if (!document.getElementById("_injected_back_nav")) {
-      const backNavEl = document.createElement("a");
-      backNavEl.setAttribute("id", "_injected_back_nav");
-      backNavEl.style.whiteSpace = "nowrap";
-      backNavEl.setAttribute("href", "/blog");
-      const text = document.createTextNode("< Blog");
-      backNavEl.append(text);
-      sidebar.appendChild(backNavEl);
-
-      backNavEl.addEventListener("click", (e) => {
-        e.preventDefault();
-
-        // hack via https://stackoverflow.com/a/62914542/39904 to trigger
-        // the docusaurus navigation. Otherwise ther
-        // simulate navigation to where you want to be (changes URL but doesn't navigate)
-        window.history.pushState("", "", "/blog");
-        // simulate navigation again so that
-        window.history.pushState("", "", "/blog");
-        // when you simulate back, the router tries to get BACK to "/url"
-        window.history.go(-1);
-      });
-    }
-
     if (blogPostRegEx.test(location.pathname) === true) {
       // Viewing an individual blog post
       main.className = "col col--8";
       sidebar.className = "col col--1 blog-left-sidebar";
+      nav.style.display = "none";
 
       const rightSidebar = document.querySelector(
         "div.col.col--2, .blog-right-sidebar"
@@ -66,12 +44,36 @@ const adjustBlogPostUI = (location) => {
         rightSidebar.className = "col col--3 blog-right-sidebar";
       }
 
-      nav.style.display = "none";
+      const backNavEl = document.getElementById("_injected_back_nav");
+      if (!backNavEl) {
+        const backNavEl = document.createElement("a");
+        backNavEl.setAttribute("id", "_injected_back_nav");
+        backNavEl.style.whiteSpace = "nowrap";
+        backNavEl.style.position = "fixed";
+        backNavEl.setAttribute("href", "/blog");
+        const text = document.createTextNode("< Blog");
+        backNavEl.append(text);
+        sidebar.appendChild(backNavEl);
+
+        backNavEl.addEventListener("click", (e) => {
+          e.preventDefault();
+
+          // hack via https://stackoverflow.com/a/62914542/39904 to trigger
+          // the docusaurus navigation. Otherwise ther
+          // simulate navigation to where you want to be (changes URL but doesn't navigate)
+          window.history.pushState("", "", "/blog");
+          // simulate navigation again so that
+          window.history.pushState("", "", "/blog");
+          // when you simulate back, the router tries to get BACK to "/url"
+          window.history.go(-1);
+
+          backNavEl.remove();
+        });
+      }
     } else {
       // Main blog listing page
       main.className = "col col--7";
       sidebar.className = "col col--3";
-
       nav.style.display = "";
     }
   } catch (ex) {
@@ -79,12 +81,15 @@ const adjustBlogPostUI = (location) => {
   }
 };
 
+// Store the last location to be used in the
+// window resize event handler
 let lastLocation = null;
 export function onRouteDidUpdate({ location }) {
   lastLocation = location;
   adjustBlogPostUI(lastLocation);
 }
 
+// Do not resize on every single event
 let resizeTimeout = null;
 window.addEventListener("resize", () => {
   if (resizeTimeout) {
