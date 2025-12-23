c4792884b4705438d00df1bf9680d357e6987ddc
David Myriel <davidmyriel@gmail.com>
2025-12-23 13:18:30 +0100

    add content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,15 +72,19 @@ const config = {
   ],
 
   plugins: [
-    [
-      "posthog-docusaurus",
-      {
-        apiKey: process.env.NEXT_PUBLIC_POSTHOG_APIKEY,
-        appUrl: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-        opt_in_site_apps: true,
-        enableInDevelopment: process.env.USE_POSTHOG_IN_DEVELOPMENT === "true",
-      },
-    ],
+    ...(process.env.NEXT_PUBLIC_POSTHOG_APIKEY
+      ? [
+          [
+            "posthog-docusaurus",
+            {
+              apiKey: process.env.NEXT_PUBLIC_POSTHOG_APIKEY,
+              appUrl: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+              opt_in_site_apps: true,
+              enableInDevelopment: process.env.USE_POSTHOG_IN_DEVELOPMENT === "true",
+            },
+          ],
+        ]
+      : []),
   ],
 
   themeConfig:
